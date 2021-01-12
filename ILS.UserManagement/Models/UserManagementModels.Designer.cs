62f694bd7490faba784f547ef60b4f830ef79bc5
qp <qp@DESKTOP-DCTEGOR>
2021-01-12 13:52:44 +0500

    Revert "Devextreme added - Working"
    This reverts commit 8c44d877b94d90c2c047d581391c85a3c57696d8.

--- ILS.UserManagement/Models/UserManagementModels.Designer.cs
+++ ILS.UserManagement/Models/UserManagementModels.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'D:\Source\Repos\Solution\ILS.UserManagement\Models\UserManagementModels.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\QVISE\source\repos\CMMS\ILS.UserManagement\Models\UserManagementModels.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
