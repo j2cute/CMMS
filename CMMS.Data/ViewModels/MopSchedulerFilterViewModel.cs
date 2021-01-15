b2cb0cb8f5da1455063a1b251dd475a837fc6ea5
qp <qp@DESKTOP-DCTEGOR>
2021-01-15 13:08:29 +0500

    Scheduler data population

---
+++ CMMS.Data/ViewModels/MopSchedulerFilterViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
+
+namespace ClassLibrary.ViewModels
+{
+    public class MopSchedulerFilterViewModel
+    {
+        [Display(Name="Start Date")]
+        public string StartDate { get; set; }
+
+        [Display(Name = "End Date")]
+        public string EndDate { get; set; }
+
+        [Display(Name = "Site Id")]
+        public string SiteId { get; set; }
+
+        [Display(Name = "MOP No")]
+        public string MopNo { get; set; }
+
+        [Display(Name = "PMS No")]
+        public string PmsNo { get; set; }
+    }
+}
