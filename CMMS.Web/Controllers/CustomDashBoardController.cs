8530145ac5ab1503e080748e1db03958ff754d64
qp <qp@DESKTOP-DCTEGOR>
2021-01-21 14:55:14 +0500

    Devexpress Dashboard with editor and viewer

---
+++ CMMS.Web/Controllers/CustomDashBoardController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplication.Controllers
+{
+    public class CustomDashBoardController : Controller
+    {
+        // GET: Dashboard
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        public ActionResult ClientMode()
+        {
+            return View("ClientMode");
+        }
+
+        public ActionResult DesignerMode()
+        {
+            return View("DesignerMode");
+        }
+    }
+}
